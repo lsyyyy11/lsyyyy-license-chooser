1d8b4bb17bef7a95130b0ab05f2d02ec786174dc
lsyyyy11 <105684171+lsyyyy11@users.noreply.github.com>
2024-06-30 14:01:13 +0800

    v2.0.0

--- scripts.js
+++ scripts.js
@@ -3,37 +3,42 @@ function chooseLicense() {
     const commercial = document.getElementById('commercial').value;
 
     let license = '';
-    let licenseUrl = '';
-    let licenseImage = '';
+    let licenseURL = '';
+    let licenseImageURL = '';
 
-    if (modification === 'yes' && commercial === 'yes') {
-        license = 'CC BY';
-        licenseUrl = 'https://creativecommons.org/licenses/by/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by/4.0/88x31.png';
-    } else if (modification === 'yes' && commercial === 'no') {
-        license = 'CC BY-NC';
-        licenseUrl = 'https://creativecommons.org/licenses/by-nc/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by-nc/4.0/88x31.png';
-    } else if (modification === 'no' && commercial === 'yes') {
-        license = 'CC BY-ND';
-        licenseUrl = 'https://creativecommons.org/licenses/by-nd/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by-nd/4.0/88x31.png';
-    } else if (modification === 'no' && commercial === 'no') {
-        license = 'CC BY-NC-ND';
-        licenseUrl = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by-nc-nd/4.0/88x31.png';
-    } else if (modification === 'share-alike' && commercial === 'yes') {
-        license = 'CC BY-SA';
-        licenseUrl = 'https://creativecommons.org/licenses/by-sa/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by-sa/4.0/88x31.png';
-    } else if (modification === 'share-alike' && commercial === 'no') {
-        license = 'CC BY-NC-SA';
-        licenseUrl = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
-        licenseImage = 'https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png';
+    if (modification === 'yes') {
+        if (commercial === 'yes') {
+            license = 'CC BY 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by/4.0/88x31.png';
+        } else {
+            license = 'CC BY-NC 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by-nc/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by-nc/4.0/88x31.png';
+        }
+    } else if (modification === 'no') {
+        if (commercial === 'yes') {
+            license = 'CC BY-ND 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by-nd/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by-nd/4.0/88x31.png';
+        } else {
+            license = 'CC BY-NC-ND 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by-nc-nd/4.0/88x31.png';
+        }
+    } else if (modification === 'share-alike') {
+        if (commercial === 'yes') {
+            license = 'CC BY-SA 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by-sa/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by-sa/4.0/88x31.png';
+        } else {
+            license = 'CC BY-NC-SA 4.0';
+            licenseURL = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+            licenseImageURL = 'https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png';
+        }
     }
 
-    document.getElementById('license-link').innerText = license;
-    document.getElementById('license-link').href = licenseUrl;
-    document.getElementById('license-image').src = licenseImage;
-    document.getElementById('license-image').alt = license;
+    document.getElementById('license-link').href = licenseURL;
+    document.getElementById('license-link').textContent = license;
+    document.getElementById('license-image').src = licenseImageURL;
 }
